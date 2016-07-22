030bb9c1e9cab4d30cf731abe5feccccbd286583
Adam Johannesmeyer <adamj0101@gmail.com>
2016-07-22 16:33:03 -0700

    sorted and organized files/folders

--- main.js
+++ main.js
@@ -12,12 +12,13 @@ var config = {
 };
 firebase.initializeApp(config);
 
+console.log(firebaseLocal)
 var database = firebase.database();
 
 function runPython() {
-  console.log('- running python... -')
-  child_process.execSync('python '+ local +'dotaApi.py', {timeout: 10000, stdio:[0,1,2]})
-  console.log('+ Python completed! +')
+  console.log('- running python... -');
+  child_process.execSync('python '+ local +'dotaApi.py', {timeout: 10000, stdio:[0,1,2]});
+  console.log('+ Python completed! +');
 }
 
 function updateDatabase() {
@@ -25,29 +26,30 @@ function updateDatabase() {
   MongoClient.connect("mongodb://127.0.0.1:27017/dota", (err, db) => {
     if(!err) {
 
-        //runs python to update mongodb database with new data
-        runPython()
+      //runs python to update mongodb database with new data
+      runPython();
 
-        let collection = db.collection('currentGame')
+      let collection = db.collection('currentGame');
 
-        collection.findOne({'_id': 1}, (err, doc) => {
-          console.log("+ updating data to firebase +")
+      collection.findOne({'_id': 1}, (err, doc) => {
+        console.log("+ updating data to firebase +");
 
-	  //not needed for web client
-          delete doc._id;
-          delete doc.league.league_id;
+        //not needed for web client
+        delete doc._id;
+        delete doc.league.league_id;
 
-          //database.ref('currentGame').set(doc);
-          console.log("- updated data to firebase -")
+        database.ref('currentGame').set(doc);
+        console.log("- updated data to firebase -");
 
-   	  db.close();
-        });
+        db.close();
+      });
     } else {
-	console.log('error with mongo');
-	console.log(err);
+      console.log('error with mongo');
+      console.log(err);
     }
   });
 }
+
 // run evey 16 seconds, api updates every 15 seconds
 // null : run on complete
 // true : start the cronjob object
